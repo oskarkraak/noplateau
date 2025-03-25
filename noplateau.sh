ab3dc0fc5311323b65e419658727aa6d57fd82ed
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2025-03-25 19:55:05 +0100

    Add accurate time tracking

--- noplateau.sh
+++ noplateau.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SECONDS=0
+
+# constants
+pynguin_time=5
+
 base_dir=$(pwd)
 echo ">>> $base_dir"
 
@@ -25,34 +30,52 @@ TIME_USED=0
 
 function pynguin {
     echo ">>> Pynguin"
+
     export PYNGUIN_DANGER_AWARE=true
     export PYTHONPATH=./src:$PYTHONPATH
-    python3.10 ./src/pynguin/__main__.py --project-path targets --module-name fun --output-path outputs --verbose --maximum-iterations 50 --initial-population-seeding True --initial_population_data targets --seed 100
+    TIME_LEFT=$((time_budget - TIME_USED))
+    if [$TIME_LEFT -lt $pynguin_time]; then
+        pynguin_time=$TIME_LEFT
+    fi
 
-    TIME_USED=$((TIME_USED + 2))
+    python3.10 ./src/pynguin/__main__.py \
+        --project-path targets \
+        --module-name fun \
+        --output-path outputs \
+        --verbose \
+        --initial-population-seeding True \
+        --initial_population_data targets \
+        --seed 0 \
+        --assertion_generation=NONE \
+        --maximum_search_time $pynguin_time
+    TIME_USED=$((TIME_USED + pynguin_time))
 }
 
 function coverup {
     echo ">>> Coverup"
     cd coverup/src/
-    python3.10 -m coverup.__main__ --source-dir targetsshort --tests-dir tests --model gpt-4o-mini --no-isolate-tests
-    cd ..
-    cd ..
+    time_before=$SECONDS
 
-    TIME_USED=$((TIME_USED + 2))
+    # TODO: make coverup take time as input argument
+    python3.10 -m coverup.__main__ --source-dir targetsshort --tests-dir tests --model gpt-4o-mini --no-isolate-tests
+    
+    time_after=$SECONDS
+    TIME_USED=$((TIME_USED + time_after - time_before))
+    cd ../..
 }
 
 # Alternating loop
 toggle=0
 while [ $TIME_USED -lt $time_budget ]; do
     if [ $toggle -eq 0 ]; then
-        pynguin
+        coverup
         toggle=1
     else
-        coverup
+        pynguin
         toggle=0
     fi    
-    echo ">>> Time used so far: $TIME_USED"
+    echo ">>> Total runtime: $SECONDS"
+    echo ">>> Time budget used so far: $TIME_USED / $time_budget"
 done
 
 echo ">>> Time budget exhausted. Exiting."
